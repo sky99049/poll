0741c164830ad6ca894446e3002779e37d3c8043
666 <sky990490111@gmail.com>
2020-01-02 12:06:14 +0800

    3

--- default/urls.py
+++ default/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('poll/', views.PollList.as_view ()),
     path('poll/<int:pk>/', views.PollDetail.as_view()),
+    path('option/<int:pk>/', views.PollVote.as_view()),
     
 ]
+
+
+
+
+
--- default/views.py
+++ default/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , render_to_response
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, RedirectView
 from .models import *
 # Create your views here.
 
@@ -13,5 +13,16 @@ class PollList(ListView):
 
 class PollDetail(DetailView):
     model = Poll
-
-
+    # 取得額外資料供頁面範本顯示
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        options = Option.objects.filter(poll_id=self.kwargs['pk'])
+        context['options'] = options
+        return context
+## 投票
+class PollVote(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        option = Option.objects.get(id=self.kwargs['pk'])
+        option.count += 1   # 將選項的票數+1
+        option.save()       # 儲存至資料庫
+        return "/poll/"+str(option.poll_id)+"/"
